8a2d0aff7b00ba87005a0f8f1177475df4f9aaf0
Marcel Zulian <marcelz.luz@gmail.com>
2023-11-07 19:20:41 -0300

    initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from 'next'
+import { Nunito_Sans } from 'next/font/google'
+import './globals.css'
+
+const nunitoSans = Nunito_Sans({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Melody Explorer',
+  description: 'Conheça mais sobre seu artista favorito',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={nunitoSans.className}>{children}</body>
+    </html>
+  )
+}
